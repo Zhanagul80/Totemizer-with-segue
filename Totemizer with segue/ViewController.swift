ab3f49181d34c0dd53c620c04d04a9e5dba80a06
Zhanagul <190107044@stu.sdu.edu.kz>
2022-04-23 12:23:07 +0600

    Initial Commit

---
+++ Totemizer with segue/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Totemizer with segue
+//
+//  Created by Zhanagul on 23.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
